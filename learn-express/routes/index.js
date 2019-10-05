839225df2b4fdbac84ea6c53ac5206341f6a9b1e
soyoung <soyoungp02@gmail.com>
2019-10-05 14:02:26 +0900

    6.5 템플릿 엔진 사용하기 실습 완료

--- learn-express/routes/index.js
+++ learn-express/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // 1. 현재 라우터에서만 title 접근 가능
+  // res.render('index', { title: 'Express' });
+
+  // 2. 다른 미들웨어에서도 res.locals 객체 접근 가능 (title 변수 사용 가능)
+  res.locals.title = 'Express';
+  res.render('index');
 });
 
 module.exports = router;
